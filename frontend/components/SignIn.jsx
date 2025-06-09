7724c8fe8d86922e6b2957b61cff7c2663ea2759
neeru0713 <neerurani1307@gmail.com>
2025-06-09 15:04:55 +0530

    developed signin component

--- frontend/components/SignIn.jsx
+++ frontend/components/SignIn.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 
-const SignUp = () => {
+const SignIn = () => {
   const [email, setEmail] = useState("");
-  const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
   const inputChangeHandler = (e) => {
     const { name, value } = e.target;
     if (name === "email") {
       setEmail(value);
-    } else if (name === "username") {
-      setUserName(value);
     } else if (name === "password") {
       setPassword(value);
     }
@@ -25,11 +22,14 @@ const SignUp = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, username, password }),
+        body: JSON.stringify({ email, password }),
       });
 
       const res = await response.json();
       console.log("......res", res);
+
+      localStorage.setItem("user", JSON.stringify(res.user));
+      localStorage.setItem("token", res.token);
     } catch (error) {
       console.error("Sign In Error", error.message);
     }
@@ -37,21 +37,13 @@ const SignUp = () => {
 
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="m-auto p-4 text-2xl font-bold text-blue-700">SignUp</h1>
+      <h1 className="m-auto p-4 text-2xl font-bold text-blue-700">SignIn</h1>
       <div className="flex flex-col w-[20%] relative gap-6 border rounded-xl border-gray-200 m-auto shadow shadow-lg p-6">
         <input
           type="email"
           name="email"
           placeholder="Email"
           value={email}
-          onChange={inputChangeHandler}
-          className="border rounded-md border-gray-300 p-2 text-sm text-gray-700 w-[90%]"
-        />
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={username}
           onChange={inputChangeHandler}
           className="border rounded-md border-gray-300 p-2 text-sm text-gray-700 w-[90%]"
         />
@@ -71,8 +63,9 @@ const SignUp = () => {
           Submit
         </button>
       </div>
-    </div>
+      </div>
+      
   );
 };
 
-export default SignUp;
+export default SignIn;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import SignUp from '../components/SingUp'
+import SignIn from "../components/SignIn";
 import Home from "../components/Home"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
@@ -8,13 +9,12 @@ function App() {
 
   return (
     <>
-    
       <Router>
         <div className="min-h-screen bg-white">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
-           
+            <Route path="/signin" element={<SignIn />} />
           </Routes>
         </div>
       </Router>
