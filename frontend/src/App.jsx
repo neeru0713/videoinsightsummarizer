f8a701da7a52a9a54300f5d748ec7f07f4409461
neeru0713 <neerurani1307@gmail.com>
2025-06-09 14:54:03 +0530

    react router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import './App.css'
 import SignUp from '../components/SingUp'
-
+import Home from "../components/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
 
 
   return (
     <>
-      <div className="bg-white w-screen h-screen" >
-        <SignUp />
-      </div>
+    
+      <Router>
+        <div className="min-h-screen bg-white">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+           
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
