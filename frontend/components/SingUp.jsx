641e22db2924085da7db773c2521f94bebbabf43
neeru0713 <neerurani1307@gmail.com>
2025-06-09 14:38:11 +0530

    developed signup page

--- frontend/components/SingUp.jsx
+++ frontend/components/SingUp.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const SingUp = () => {
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const inputChangeHandler = (e) => {
+    const { name, value } = e.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const res = await response.json();
+      console.log("......res", res);
+    } catch (error) {
+      console.error("Sign In Error", error.message);
+    }
+  };
+
   return (
-    <div>SingUp</div>
-  )
-}
+    
+      <div className="flex flex-col w-[20%] relative gap-6 border rounded-xl border-gray-200 m-auto shadow shadow-lg mt-[5%] p-6">
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={inputChangeHandler}
+          className="border rounded-md border-gray-300 p-2 text-sm text-gray-700 w-[90%]"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={inputChangeHandler}
+          className="border rounded-md border-gray-300 p-2 text-sm text-gray-700 w-[90%]"
+        />
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded-md cursor-pointer"
+        >
+          Submit
+        </button>
+      </div>
+   
+  );
+};
 
-export default SingUp
+export default SignUp;
