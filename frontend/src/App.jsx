77e3d1eb13100966aa1419b9e4959d257d73993b
neeru0713 <neerurani1307@gmail.com>
2025-06-10 18:26:01 +0530

    Did route setup of Adminpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './App.css'
 import SignUp from '../components/SingUp'
 import SignIn from "../components/SignIn";
-import Home from "../components/Home"
+import Home from "../components/Home";
+import Admin from "../components/Admin";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/signin" element={<SignIn />} />
+            <Route path="/admin" element={<Admin />} />
           </Routes>
         </div>
       </Router>
